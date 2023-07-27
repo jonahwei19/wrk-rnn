2eafbc81c9c80dae6bc6a2991e0231144d8c4a51
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-27 03:17:13 -0400

    Update Survey.tsx

--- screens/Survey.tsx
+++ screens/Survey.tsx
@@ -3,6 +3,9 @@ import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import axios from 'axios';
 import WebView from 'react-native-webview';
 
+const Chat = () => {
+  // You can put your existing state and functions here...
+
   return (
     <View style={styles.container}>
       <WebView source={{ uri: 'https://jonahwei19-investormatch-streamlit-app-lipv2u.streamlit.app/' }} />
@@ -10,5 +13,7 @@ import WebView from 'react-native-webview';
     </View>
   );
 };
+
+export default Chat;
 
 // ... your existing code ...
