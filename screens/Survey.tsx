4c8acc01483b89477d77738efc91af22977be13d
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-27 03:33:55 -0400

    Update Survey.tsx

--- screens/Survey.tsx
+++ screens/Survey.tsx
@@ -1,22 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import axios from 'axios';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  // You can add more styles here...
-});
 
 const Chat = () => {
   // You can put your existing state and functions here...
 
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <iframe src="https://jonahwei19-investormatch-streamlit-app-lipv2u.streamlit.app/" width="100%" height="500px"></iframe>
+      <iframe src="https://jonahwei19-investormatch-streamlit-app-lipv2u.streamlit.app/" style={{width: '100%', height: '500px'}}></iframe>
       {/* ... your existing code ... */}
     </div>
   );
