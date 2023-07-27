e2bb7328c801eac9ee2e2df0d41982908833c92d
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-27 03:04:02 -0400

    Update Survey.tsx

--- screens/Survey.tsx
+++ screens/Survey.tsx
@@ -1,63 +1,179 @@
-import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
-import axios from 'axios';
+import {useState} from 'react';
+import axios from "axios";
+import PromptInput from "../PromptInput/PromptInput";
+import './App.css';
+import {ResponseInterface} from "../PromptResponseList/response-interface";
+import PromptResponseList from "../PromptResponseList/PromptResponseList";
 
-const openai = axios.create({
-  baseURL: 'https://api.openai.com/v1',
-  headers: {
-    'Authorization': `Bearer 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`,
-    'Content-Type': 'application/json'
+type ModelValueType = 'gpt' | 'codex' | 'image';
+const App = () => {
+
+  const [responseList, setResponseList] = useState<ResponseInterface[]>([]);
+  const [prompt, setPrompt] = useState<string>('');
+  const [promptToRetry, setPromptToRetry] = useState<string | null>(null);
+  const [uniqueIdToRetry, setUniqueIdToRetry] = useState<string | null>(null);
+  const [modelValue, setModelValue] = useState<ModelValueType>('gpt');
+  const [isLoading, setIsLoading] = useState(false);
+  let loadInterval: number | undefined;
+
+  const generateUniqueId = () => {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
   }
-});
 
-const Chat = () => {
-  const [messages, setMessages] = useState([
-    {role: 'system', content: 'You are a helpful assistant.'}
-  ]);
-  const [input, setInput] = useState('');
+  const htmlToText = (html: string) => {
+    const temp = document.createElement('div');
+    temp.innerHTML = html;
+    return temp.textContent;
+  }
 
-  const sendMessage = async () => {
-    const newMessages = [...messages, {role: 'user', content: input}];
-    setMessages(newMessages);
-    setInput('');
+  const delay = (ms: number) => {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
 
-    const response = await openai.post('/engines/davinci-codex/completions', {
-      model: 'text-davinci-002',
-      messages: newMessages
+  const addLoader = (uid: string) => {
+    const element = document.getElementById(uid) as HTMLElement;
+    element.textContent = ''
+
+    // @ts-ignore
+    loadInterval = setInterval(() => {
+      // Update the text content of the loading indicator
+      element.textContent += '.';
+
+      // If the loading indicator has reached three dots, reset it
+      if (element.textContent === '....') {
+        element.textContent = '';
+      }
+    }, 300);
+  }
+
+
+  const addResponse = (selfFlag: boolean, response?: string) => {
+    const uid = generateUniqueId()
+    setResponseList(prevResponses => [
+      ...prevResponses,
+      {
+        id: uid,
+        response,
+        selfFlag
+      },
+    ]);
+    return uid;
+  }
+
+  const updateResponse = (uid: string, updatedObject: Record<string, unknown>) => {
+    setResponseList(prevResponses => {
+      const updatedList = [...prevResponses]
+      const index = prevResponses.findIndex((response) => response.id === uid);
+      if (index > -1) {
+        updatedList[index] = {
+          ...updatedList[index],
+          ...updatedObject
+        }
+      }
+      return updatedList;
     });
+  }
 
-    const assistantMessage = response.data.choices[0].text.trim();
-    setMessages([...newMessages, {role: 'assistant', content: assistantMessage}]);
-  };
+  const regenerateResponse = async () => {
+    await getGPTResult(promptToRetry, uniqueIdToRetry);
+  }
+
+  const getGPTResult = async (_promptToRetry?: string | null, _uniqueIdToRetry?: string | null) => {
+    // Get the prompt input
+    const _prompt = _promptToRetry ?? htmlToText(prompt);
+
+    // If a response is already being generated or the prompt is empty, return
+    if (isLoading || !_prompt) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    // Clear the prompt input
+    setPrompt('');
+
+    let uniqueId: string;
+    if (_uniqueIdToRetry) {
+      uniqueId = _uniqueIdToRetry;
+    } else {
+      // Add the self prompt to the response list
+      addResponse(true, _prompt);
+      uniqueId = addResponse(false);
+      await delay(50);
+      addLoader(uniqueId);
+    }
+
+    try {
+      // Send a POST request to the API with the prompt in the request body
+      const response = await axios.post('get-prompt-result', {
+        prompt: _prompt,
+        model: modelValue
+      });
+      if (modelValue === 'image') {
+        // Show image for `Create image` model
+        updateResponse(uniqueId, {
+          image: response.data,
+        });
+      } else {
+        updateResponse(uniqueId, {
+          response: response.data.trim(),
+        });
+      }
+
+      setPromptToRetry(null);
+      setUniqueIdToRetry(null);
+    } catch (err) {
+      setPromptToRetry(_prompt);
+      setUniqueIdToRetry(uniqueId);
+      updateResponse(uniqueId, {
+        // @ts-ignore
+        response: `Error: ${err.message}`,
+        error: true
+      });
+    } finally {
+      // Clear the loader interval
+      clearInterval(loadInterval);
+      setIsLoading(false);
+    }
+  }
 
   return (
-    <View style={styles.container}>
-      {messages.map((message, index) => (
-        <Text key={index}>{message.role}: {message.content}</Text>
-      ))}
-      <TextInput
-        style={styles.input}
-        value={input}
-        onChangeText={setInput}
-        placeholder="Type a message"
-      />
-      <Button title="Send" onPress={sendMessage} />
-    </View>
+    <div className="App">
+      <div id="response-list">
+        <PromptResponseList responseList={responseList} key="response-list"/>
+      </div>
+      { uniqueIdToRetry &&
+        (<div id="regenerate-button-container">
+          <button id="regenerate-response-button" className={isLoading ? 'loading' : ''} onClick={() => regenerateResponse()}>
+            Regenerate Response
+          </button>
+        </div>
+        )
+      }
+      <div id="model-select-container">
+        <label htmlFor="model-select">Select model:</label>
+        <select id="model-select" value={modelValue} onChange={(event) => setModelValue(event.target.value as ModelValueType)}>
+          <option value="gpt">GPT-3 (Understand and generate natural language )</option>
+          <option value="codex">Codex (Understand and generate code, including translating natural language to code)
+          </option>
+          <option value="image">Create Image (Create AI image using DALL·E models)</option>
+        </select>
+      </div>
+      <div id="input-container">
+        <PromptInput
+          prompt={prompt}
+          onSubmit={() => getGPTResult()}
+          key="prompt-input"
+          updatePrompt={(prompt) => setPrompt(prompt)}
+        />
+        <button id="submit-button" className={isLoading ? 'loading' : ''} onClick={() => getGPTResult()}></button>
+      </div>
+    </div>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginTop: 20,
-  },
-});
-
-export default Chat;
+export default App;
