83c10212494fede3eb9d21e09cd94a6e69f6f56f
jonahwei19 <125212703+jonahwei19@users.noreply.github.com>
2023-07-28 15:59:13 -0400

    Update Survey.tsx

--- screens/Survey.tsx
+++ screens/Survey.tsx
@@ -2,12 +2,54 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Chat = () => {
-  // You can put your existing state and functions here...
+  const [message, setMessage] = useState("");
+  const [conversation, setConversation] = useState([]);
+
+  const getGptResponse = async () => {
+    const data = {
+      model: "gpt-3.5-turbo",
+      messages: [...conversation, {role: "user", content: message}],
+      max_tokens: 1000,
+      temperature: 0.5,
+    };
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer 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`
+      }
+    };
+
+    try {
+      const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', data, config);
+      const gptResponse = response.data.choices[0].message.content;
+      setConversation([...conversation, {role: "assistant", content: gptResponse}]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getGptResponse();
+    setMessage("");
+  };
 
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <iframe src="https://jonahwei19-investormatch-streamlit-app-lipv2u.streamlit.app/" style={{width: '100%', height: '500px'}}></iframe>
-      {/* ... your existing code ... */}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea value={message} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {conversation.map((message, index) => (
+          <p key={index}><strong>{message.role}:</strong> {message.content}</p>
+        ))}
+      </div>
     </div>
   );
 };
